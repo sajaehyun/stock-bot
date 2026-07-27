e33353ef256be926ce345d57f00e9d885fbee92c
sajaehyun <nicesade@gmail.com>
2026-07-27 14:55:44 +0900

    backtest.py 업데이트

--- backtest.py
+++ backtest.py
@@ -7,7 +7,7 @@ import os, json, pathlib, logging
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
-import pandas_datareader as pdr
+
 
 LOG = logging.getLogger("backtest")
 HISTORY_DIR   = pathlib.Path("history")
